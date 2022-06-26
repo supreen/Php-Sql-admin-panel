154f2a638e313f91530100efa6ed9489dbc83182
Suprin Aziz <62968684+supreen@users.noreply.github.com>
2022-06-26 18:36:33 +0500

    Add files via upload

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "<SQL SERVER NAME>";
+$username = "<SQL USERNAME>";
+$password = "<SQL PASSWORD>";
+$dbname = "<SQL DATABASE NAME>";
+// you can get the sitekeyfrom https://www.google.com/recaptcha/admin/create . make sure that it is captcha v2 and i am not robot. 
+$sitekey="<CAPTCHA SITEKEY>";
+// you can get the secret from https://www.google.com/recaptcha/admin/create . make sure that it is captcha v2 and i am not robot. 
+$secret="<CAPTCHA SECRET>";
+?>
